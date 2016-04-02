e967cfbf92279a3966cb2ee0f9d0649b7bd39a6a
Chris Belsole <cbelsole@gmail.com>
2016-04-01 20:04:28 -0400

    fixed header bar and added links for all types of users

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  def header_logo_link(user)
+    if user.try(:host?)
+      games_path
+    elsif user.try(:player?)
+      find_games_path
+    else
+      root_path
+    end
+  end
+
 end
